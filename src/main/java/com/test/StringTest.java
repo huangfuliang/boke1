b9c24482fe7cb4504f70e1fe314b01f6e954d34a
huangfuliang <huangfl6@asiainfo.com>
2017-11-20 23:08:25 +0800

    Initial commit

---
+++ src/main/java/com/test/StringTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * Created by huangfuliang on 2017/11/20.
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String str1 = new String("abc");
+        String str2 = new String("abc");
+        System.out.println(str1 == str2);
+    }
+}
